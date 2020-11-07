27dd2125d97de4b7a93afc3633846b3d6c52a682
Лидия <lidia@MacBook-Pro-Lidia.local>
2020-11-07 16:29:11 +0300

    :tada:

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "@emotion/styled";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IProps {}
+
+const Root = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const App: React.FC<IProps> = () => {
+  return <Root>hello</Root>;
 }
-
 export default App;
